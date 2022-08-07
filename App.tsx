c6a0895c2cd0ffae7a6e3d65608241f290d574d1
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2022-08-07 21:46:16 +0300

    init commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.formContainer}>
+        <Text>Get started with Finance app</Text>
+        <TextInput placeholder="email" style={styles.input} accessibilityHint="email" accessibilityLabel="email" />
+        <TextInput placeholder="password" style={styles.input} />
+        <Pressable
+          style={({ pressed }) => (pressed ? styles.pressedItem : styles.button)}
+          android_ripple={{ color: "tortoise" }}
+        >
+          <Text style={styles.btnText}>login</Text>
+        </Pressable>
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#E8F9FD",
+    justifyContent: "center",
+  },
+
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    height: 200,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    width: 280,
+    height: 50,
+    paddingLeft: 10,
+  },
+
+  button: {
+    marginTop: 5,
+    width: 280,
+    height: 40,
+    backgroundColor: "#59CE8F",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  btnText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+
+  pressedItem: {
+    backgroundColor: "#3FA796",
+    marginTop: 5,
+    width: 280,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
